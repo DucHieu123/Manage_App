4ccbe12ca71dbf190773e46f5ae4f8e7010ee389
Bùi quốc đạt <qdat19051998@gmail.com>
2019-08-21 07:03:20 +0700

    Bùi quốc đạt chức năng sửa sản phẩm

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -1,33 +1,44 @@
 package com.example.news;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.news.adapter.CustomAdapter;
+import com.example.news.adapter.DBManager;
+import com.example.news.model.SanPham;
+import com.example.news.untils.Common;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private ListView lvMonHoc;
+    private ListView lvSanPham;
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private DrawerLayout dl;
     private ActionBarDrawerToggle t;
     private NavigationView nv;
-
-
+    DBManager dbManager;
+    ArrayList<SanPham> arrSanPham;
+    CustomAdapter customAdaper;
+    int dem = 0;
     private static final int REQUEST_CODE_QR_SCAN = 101;
     private final String LOGTAG = "QRCScanner-MainActivity";
 
@@ -37,17 +48,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lvMonHoc = (ListView) findViewById(R.id.lvmonhoc);
-        ArrayList<MonHoc> arrMonhoc = new ArrayList<>();
-        for (int i = 0; i < 30; i++) {
-            arrMonhoc.add(new MonHoc("Môn học " + i, "ca " + i));
-        }
+        lvSanPham = (ListView) findViewById(R.id.lvsanpham);
 
-        CustomAdapter customAdaper = new CustomAdapter(this, R.layout.row_listview_monhoc, arrMonhoc);
-        lvMonHoc.setAdapter(customAdaper);
+        dbManager = new DBManager(getApplicationContext());
+        arrSanPham = dbManager.getSanPham();
 
 
+        customAdaper = new CustomAdapter(this, R.layout.row_listview_monhoc, arrSanPham);
+        lvSanPham.setAdapter(customAdaper);
 
+        // xóa sản phẩm
+        lvSanPham.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                dbManager.xoaSanPham(arrSanPham.get(dem));
+                arrSanPham.remove(dem);
+                arrSanPham = dbManager.getSanPham();
+                customAdaper = new CustomAdapter(getApplicationContext(), R.layout.row_listview_monhoc, arrSanPham);
+                lvSanPham.setAdapter(customAdaper);
+                customAdaper.notifyDataSetChanged();
+                Toast.makeText(MainActivity.this, "Xóa Thành Công.!", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        lvSanPham.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(), SuaSanPhamActivity.class);
+                intent.putExtra("id", arrSanPham.get(i).getId());
+                intent.putExtra("ten", arrSanPham.get(i).getTensp());
+                intent.putExtra("hinhanh", Common.getBytes(arrSanPham.get(i).getHinhanh()));
+                startActivity(intent);
+            }
+        });
         //set navication bar
         dl = (DrawerLayout) findViewById(R.id.activity_main);
         t = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
@@ -63,16 +97,21 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
                 switch (id) {
-                    case R.id.somi:
-                        Toast.makeText(MainActivity.this, "Chức năng đang làm", Toast.LENGTH_SHORT).show();
+                    case R.id.themsanpham:
+                        Intent intent = new Intent(getApplicationContext(), ThemSanPhamActivity.class);
+                        startActivity(intent);
+                      
                         break;
-                    case R.id.aothun:
-                        Toast.makeText(MainActivity.this, "Chức năng đang làm", Toast.LENGTH_SHORT).show();
-                        // Intent intentSetting = new Intent(MainActivity.this,SettingActivity.class);
-                        // startActivity(intentSetting);
+                    case R.id.themloai:
+                        Intent intentSetting = new Intent(MainActivity.this,ThemLoaiSanPHamActivity.class);
+                        startActivity(intentSetting);
+
                         break;
-                    case R.id.quan:
-                        Toast.makeText(MainActivity.this, "Chức năng đang làm", Toast.LENGTH_SHORT).show();
+                    case R.id.lammoi:
+                        arrSanPham = dbManager.getSanPham();
+                        customAdaper = new CustomAdapter(getApplicationContext(), R.layout.row_listview_monhoc, arrSanPham);
+                        lvSanPham.setAdapter(customAdaper);
+                        customAdaper.notifyDataSetChanged();
                         break;
                     default:
                         break;
@@ -94,4 +133,24 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onBackPressed() {
+        arrSanPham = dbManager.getSanPham();
+        customAdaper.notifyDataSetChanged();
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onRestart() {
+        arrSanPham = dbManager.getSanPham();
+        customAdaper.notifyDataSetChanged();
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        arrSanPham = dbManager.getSanPham();
+        customAdaper.notifyDataSetChanged();
+        super.onResume();
+    }
 }
